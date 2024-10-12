7a837fc044777dca9942d18ce376997e94a91786
Luiz Eduardo <duduamaralspereira@gmail.com>
2024-10-12 14:06:36 -0300

    Animei o texto da home porém o card que adicionei não esta funcionando

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { View, Text, Image } from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import { View, Text, Image, Animated, Easing, Linking, TouchableOpacity,ScrollView } from 'react-native';
 import { useFonts } from 'expo-font'; 
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
@@ -14,6 +14,8 @@ const Tab = createMaterialTopTabNavigator();
 export default function App() {
   let [fontsLoaded] = useFonts({
     'Lobster': require('./assets/fonts/Lobster-Regular.ttf'),
+    "Roboto" : require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto" : require("./assets/fonts/Roboto-Bold.ttf")
   });
 
   if (!fontsLoaded) {
@@ -25,7 +27,7 @@ export default function App() {
       <Tab.Navigator
         screenOptions={{
           tabBarStyle: { backgroundColor: '#ff6c50' }, 
-          tabBarLabelStyle: { color: '#fff', fontSize: 14,  fontFamily: 'Lobster',}, 
+          tabBarLabelStyle: { color: '#fff', fontSize: 14,  fontFamily: 'Roboto',}, 
           tabBarIndicatorStyle: { backgroundColor: '#fff' }, 
         }}
       >
@@ -42,33 +44,60 @@ function HomeScreen() {
   const texto = "Tia Quero Doce é sinônimo de sabor e carinho. Diretamente de Madureira, nossa confeiteira começou com uma paixão por doces caseiros, conquistando o paladar de quem provava suas criações. Com uma tradição que une afeto e dedicação, ela traz à mesa doces que são mais que sobremesas, são memórias doces que ficam para sempre.";
   const linhas = texto.split(",");
 
+  const slideAnim = useRef(new Animated.Value(300)).current;
+  const cardAnim = useRef(new Animated.Value(300)).current;
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(slideAnim, {
+        toValue: 0,
+        duration: 2000,
+        easing: Easing.out(Easing.ease),
+        useNativeDriver: true
+      }),
+      Animated.timing(cardAnim, {
+        toValue: 0,
+        duration: 2000,
+        easing: Easing.out(Easing.ease),
+        useNativeDriver: true
+      })
+    ]).start();
+  }, [slideAnim, cardAnim]);
+
+  const abrirFacebook = () => {
+    Linking.openURL("https://www.facebook.com/share/fVr6xyh5SEoH7t65/?mibextid=qi2Omg");
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tia Quero Doce!</Text>
-      <Image
-      source={require("../tiaQueroDoce/assets/img/cupcake.png")}
-      style={styles.logo} />
-
-      <View style={styles.infoConfeiteira}>
-        <Text style={styles.title2}>Conheça nossa confeiteira</Text>
-
-        <View style={styles.secaoTexto1}>
-          <Image 
+      <Animated.View style={[styles.containerAnimado, { transform: [{ translateY: slideAnim }] }]}>
+        <Text style={styles.title}>Tia Quero Doce!</Text>
+        <Image
           source={require("../tiaQueroDoce/assets/img/ConfeiteiraIlustracao.png")}
-          style={styles.confeiteiraIlustracao1}
-          />
+          style={styles.logo} />
 
-          <View style={styles.divTexto}>
-            {linhas.map((linha, index) => (
-            <Text key={index} style={[styles.texto, { marginLeft: (index % 2 === 0) ? 5 : 25 }]}>
+        <View style={styles.infoConfeiteira}>
+          <Text style={styles.title2}>Conheça nossa confeiteira</Text>
+          {linhas.map((linha, index) => (
+            <Text key={index} style={[styles.texto, { marginLeft: (index % 2 === 0) ? 10 : 50 }]}>
               {linha.trim() + (index < linhas.length - 1 ? "," : ".")}
             </Text>
-            ))}
-          </View>
+          ))}
         </View>
-        
-      </View>
+      </Animated.View>
+
+      <Animated.View style={[styles.cardAnimado, { transform: [{translateX: cardAnim}]}]}>
+          <View style={styles.cardHome}>
+            <Image
+             source={require("./assets/img/BoloDeChocolateBrancoEtTopoAzul.jpeg")}
+             style={styles.boloCard}/>
+            <TouchableOpacity onPress={abrirFacebook}>
+              <Text style={styles.link}>Visite o facebook da Tia!</Text>
+            </TouchableOpacity>
+          </View>
+      </Animated.View>
     </View>
   );
 }
 
+///Tia Quero Doce é sinônimo de sabor e carinho. Diretamente de Madureira, nossa confeiteira começou com uma paixão por doces caseiros, conquistando o paladar de quem provava suas criações. Com uma tradição que une afeto e dedicação, ela traz à mesa doces que são mais que sobremesas, são memórias doces que ficam para sempre.
--- assets/pages/home.estilos.js
+++ assets/pages/home.estilos.js
@@ -6,43 +6,55 @@ export const styles = StyleSheet.create({
       alignItems: 'center',
       backgroundColor: '#fef7da',
     },
+    containerAnimado: {
+      alignItems: "center"
+    },
     title: {
-      marginTop: 5,
+      margin: 20,
       color: "#ff6c50",
-      fontSize: 20,
+      fontSize: 25,
       fontFamily: 'Lobster',  
     },
     logo: {
-      height: 150,
-      width: 150,
-      marginBottom: 5
+      height: 280,
+      width: 280,
+      marginBottom: 20,
+      resizeMode: "contain"
     }, 
     infoConfeiteira: {
         flex: 1,
-        alignItems: "center",
-        width: "90%"
+        alignItems: "flex-start",
+        flexDirection: "column",
+        margin: 10
     },
     title2: {
       fontFamily: "Lobster",
-      fontSize: 18,
+      fontSize: 23,
       color: "#ff6c50",
-      marginBottom:10
-    },
-    secaoTexto1: {
-      flexDirection: "row",
-      alignItems: "flex-start",
-      marginLeft: 5
-    },
-    confeiteiraIlustracao1: {
-      height: 150,
-      width: 150,
-      resizeMode: "contain",
-    },
-    divTexto: {
-      flexShrink: 1
+      marginBottom: 18
     },
     texto: {
       color: "#ff6c50",
       fontFamily: "Lobster",
+      fontSize: 18
+    }
+    ,cardAnimado: {
+      width: "85%",
+      height: 400,
+      backgroundColor: "#fff",
+      borderRadius: 10,
+      alignItems: "flex-start"
+    },
+    cardHome: {
+      flex: 1,
+    },
+    boloCard: {
+      height: 50,
+      width: 50,
+    },
+    link: {
+      color: "#ff6c50",
+      fontFamily: "Lobster",
+      fontSize: 18
     }
   });
